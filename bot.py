8c2577da3e4cbf4d0924cdacc393f4fa53ee6980
Teoh Xu En <projectxuen@gmail.com>
2020-08-15 22:23:24 +0800

    refactor code into 2 files

---
+++ bot.py
@@ -0,0 +1,59 @@
+from scheduler import *
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+def botinit(update,context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi I'm a bot that help to generate schedules to help you visualise better. Call '/help' for instructions!")
+
+def bothelp(update, context):
+    message = 'Please enter your message as shown:\n' +\
+            '/make\n'+\
+            '<calendar start><calendar end>\n'+\
+            '<vehicles>\n'+\
+            '<event start>,<veh used>:<event name>,<event end>\n'+\
+            'Additionally, you can omit the event end date if the event is a 1 day event.'
+    example = '/make\n'+\
+            '10 Aug 20 24 Aug 20\n'+\
+            'Car 1, Car 2, Car 3, Car 4\n'+\
+            '11 Aug, Car 1: Quarterly Checks, 13 Aug\n'+\
+            '17 Aug, Car 2: Annual Checks\n'+\
+            '18 Aug, Car 3: Quarterly Checks, 19 Aug\n'+\
+            '20 Aug, Car 4: Annual Checks\n'+\
+            '24 Aug, Holiday!'
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Here's an example: ")
+    context.bot.send_message(chat_id=update.effective_chat.id, text=example)
+
+def botschedule(update,context):
+    filepath = scheduler(update.message.text, update.effective_chat.id, context)
+    if filepath:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Here's your picture!")
+        context.bot.send_photo(chat_id=update.effective_chat.id, photo=open(f'{filepath}/out{update.effective_chat.id}.jpg', 'rb'))
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Unfortunately we are unable to generate the schedule, check out /help for assistance")
+
+def main():
+
+    updater = Updater(token='***REMOVED***', use_context=True)
+    dispatcher = updater.dispatcher
+
+    start_handler = CommandHandler('start', botinit)
+    dispatcher.add_handler(start_handler) 
+
+    help_handler = CommandHandler('help', bothelp)
+    dispatcher.add_handler(help_handler) 
+
+    scheduler_handler = CommandHandler('make', botschedule)
+    dispatcher.add_handler(scheduler_handler)
+
+    unknown_handler = MessageHandler(Filters.command, unknown)
+    dispatcher.add_handler(unknown_handler)
+
+    updater.start_polling()
+
+    print("Bot is running! BEEP BOOP!")  
+
+
+main()
--- scheduler.py
+++ scheduler.py
@@ -4,32 +4,6 @@ import imgkit
 import os   
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-def unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
-
-def botinit(update,context):
-    # todo explain how to use the bot
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi I'm a bot that help to generate schedules to help you visualise better. Call '/help' for instructions!")
-
-def bothelp(update, context):
-    message = 'Please enter your message as shown:\n' +\
-            '/make\n'+\
-            '<calendar start><calendar end>\n'+\
-            '<serviceable ***REMOVED***s>\n'+\
-            '<event start>,<***REMOVED*** used><event name>,<event end>\n'+\
-            'Additionally, you can omit the event end date if the event is a 1 day event.'
-    example = '/make\n'+\
-            '10 Aug 20 24 Aug 20\n'+\
-            '***REMOVED***, ***REMOVED***, ***REMOVED***, ***REMOVED***\n'+\
-            '11 Aug, ***REMOVED*** ***REMOVED***, 13 Aug\n'+\
-            '17 Aug, ***REMOVED*** ***REMOVED***\n'+\
-            '18 Aug, ***REMOVED*** ***REMOVED***, 19 Aug\n'+\
-            '20 Aug, O1 ***REMOVED***\n'+\
-            '24 Aug, ***REMOVED***'
-    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Here's an example: ")
-    context.bot.send_message(chat_id=update.effective_chat.id, text=example)
-
 def caldates(data):
     try:
         dates = data.split()
@@ -42,8 +16,8 @@ def caldates(data):
         endday = int(dates[3])
         start = date(startyear, startmonth, startday)
         end = date(endyear, endmonth, endday)
-        start -= timedelta(days=start.weekday()) #change the starting date to the week monday
-        end += timedelta(days=6-end.weekday())  #change the ending date to the week sunday
+        start -= timedelta(days=start.weekday()) #change the starting day of the week to monday
+        end += timedelta(days=6-end.weekday())  #change the ending day of the week to sunday
     except:
         return False, False
     return start, end
@@ -55,7 +29,7 @@ def parseevent(data, svc, calstart, calend):
         months ={"JAN":1, "FEB":2, "MAR":3, "APR":4, "MAY":5, "JUN":6, "JUL":7, "AUG":8, "SEP":9, "OCT":10, "NOV":11, "DEC":12}
         start = info[0].split()
         name = info[1]
-        if len(info) == 2: #if event end on the same day just put the event start date can le
+        if len(info) == 2: #if event ends on the same day, only event start date is required
             end = start
         else:
             end = info[2].split()
@@ -74,9 +48,9 @@ def parseevent(data, svc, calstart, calend):
             endyear = calstart.year 
         startdate = date(startyear, startmonth, startday)
         enddate = date(endyear, endmonth, endday)
-        temp = name.split()
-        ***REMOVED*** = temp[0]
-        eventinfo = [startdate, enddate, ***REMOVED***, name]
+        temp = name.split(":")
+        veh = temp[0]
+        eventinfo = [startdate, enddate, veh, name]
     except:
         return False
     return eventinfo
@@ -95,9 +69,8 @@ def createcal(start, end, svc):
     calendar = []
     while start <= end:
         currdate = []
-        #currdate.append(tempdate)
         currdate.append(start.strftime("%d %b")) #format into a readable date
-        currdate.append(svc.copy()) # list of svc ***REMOVED***
+        currdate.append(svc.copy()) # list of veh
         currdate.append([]) # list of events
         calendar.append(currdate)
         start += timedelta(days = 1) # increment date
@@ -114,25 +87,17 @@ def updatecal(start, end, events, calendar):
         else:
             endday = (event[1] - start).days
         for i in range(startday, endday + 1):
-            calendar[i][2].append(event[3]) #add name of event to list of event on that day
-            if event[2] in calendar[i][1]: # if ***REMOVED*** under servicing is one of the svc ***REMOVED***
+            calendar[i][2].append(event[3]) # add name of event to list of event on that day
+            if event[2] in calendar[i][1]: # if vehicle under servicing is one of the veh being tracked
                 calendar[i][1].remove(event[2])
     return calendar
 
 
 def scheduler(message, chatid, context):
-    #Input will be start date, end date of calendars
+    # Input will be start date, end date of calendars
     # monday is the first day of the week
     # months "jan feb mar apr may jun jul aug sept oct nov dec"
-    # <start date of cal><end date of cal>
-    # <serviceable ***REMOVED*** ordered in priority>
-    # <start date> <***REMOVED***> <name of event> <end date>
-    # for now just generate the image
-    # future idea: show 2 weekly and send reminders, ixxf schedule
-    # add in options to show aros priority, or when aros is up
-    # better error messages and error handling
-    # better handling of dates
-    # merge cells
+    # TODO: Merge cells if they have the same event
 
     script_dir = os.path.dirname(os.path.realpath("__file__"))
     rel_dir = "results"
@@ -143,14 +108,13 @@ def scheduler(message, chatid, context):
 
     data = message.split("\n")
 
-    data.pop(0) #remove /make
+    data.pop(0) #remove '/make'
 
     if len(data) <= 2:
-        context.bot.send_message(chat_id=chatid, text="Error: Message invalid, are you missing the list of serviceable ***REMOVED***?")
+        context.bot.send_message(chat_id=chatid, text="Error: Message invalid, are you missing the list of vehicles?")
         return False
 
     # get the actual start and end date of the calendar
-    # find if the start date is mon
     start, end = caldates(data[0])
     if not start:
         context.bot.send_message(chat_id=chatid, text="Error: Calendar Start/End date is invalid or missing")
@@ -160,16 +124,12 @@ def scheduler(message, chatid, context):
         context.bot.send_message(chat_id=chatid, text="Error: Calendar start date is after its end date")
         return False
 
-    if (end-start).days > 105:
+    if (end-start).days > 105: # to ensure gap between calendar start and end date is within 3 months
         context.bot.send_message(chat_id=chatid, text="Error: Calendar start date and end date differ by more than 3 months")
         return False
 
-    # get the svc ***REMOVED***
+    # get the veh
     svc = [a.strip() for a in data[1].split(',')]
-    for i in svc: # check if list of serviceable ***REMOVED*** is valid
-        if i[:1] != "A" or len(i) != 2:
-            context.bot.send_message(chat_id=chatid, text="List of serviceable ***REMOVED*** is invalid")
-            return False
 
     # list of events
     events = []
@@ -209,15 +169,15 @@ def scheduler(message, chatid, context):
                     file.write(f"<div> {event}</div>")
                 file.write("</td>")
         file.write("</tr>")
-
-        # write the svc ***REMOVED***
+        
+        # write the available veh
         file.write("<tr>")
         for day in range(7): 
-            if calendar[week+day][1] == "": # if no ***REMOVED***s
+            if calendar[week+day][1] == "": # if no veh
                 file.write(f"<td>  </td>")
             else:
-                ***REMOVED***s = ", ".join(calendar[week+day][1]) # join the list of ***REMOVED*** into a string
-                file.write(f"<td> {***REMOVED***s} </td>")
+                veh = ", ".join(calendar[week+day][1]) # join the list of veh into a string
+                file.write(f"<td> {veh} </td>")
         file.write("</tr>") 
 
     file.write("</table>")
@@ -228,35 +188,3 @@ def scheduler(message, chatid, context):
 
     imgkit.from_file(f'{filepath}/table{chatid}.html', f'{filepath}/out{chatid}.jpg')
     return filepath
-
-def botschedule(update,context):
-    filepath = scheduler(update.message.text, update.effective_chat.id, context)
-    if filepath:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Here's your picture!")
-        context.bot.send_photo(chat_id=update.effective_chat.id, photo=open(f'{filepath}/out{update.effective_chat.id}.jpg', 'rb'))
-    else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Unfortunately we are unable to generate the schedule, check out /help for assistance")
-
-def main():
-
-    updater = Updater(token='***REMOVED***', use_context=True)
-    dispatcher = updater.dispatcher
-
-    start_handler = CommandHandler('start', botinit)
-    dispatcher.add_handler(start_handler) 
-
-    help_handler = CommandHandler('help', bothelp)
-    dispatcher.add_handler(help_handler) 
-
-    scheduler_handler = CommandHandler('make', botschedule)
-    dispatcher.add_handler(scheduler_handler)
-
-    unknown_handler = MessageHandler(Filters.command, unknown)
-    dispatcher.add_handler(unknown_handler)
-
-    updater.start_polling()
-
-    print("Bot is running! BEEP BOOP!")  
-
-
-main()
